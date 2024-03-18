1a0102d1119d2d4ae1a3b90ab9ab66d4ef05cb11
NurinAzhar <nabazhar1@sheffield.ac.uk>
2024-03-18 16:26:11 +0000

    Created texture assets and game menu activation

---
+++ Assets/Scripts/GameMenuOpen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuOpen : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("M key pressed");
+
+            if (menu.activeInHierarchy)
+            {
+                menu.SetActive(false);
+            } 
+            else
+            {
+                menu.SetActive(true);
+            }
+        }
+    }
+}
