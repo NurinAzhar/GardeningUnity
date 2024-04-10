6b7d32ea7a72510ec3630780660daaf593875881
NurinAzhar <nabazhar1@sheffield.ac.uk>
2024-04-10 14:07:53 +0100

    Pest Generation Singular 1,1

---
+++ Assets/Scripts/PestGenerator.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PestGenerator : MonoBehaviour
+{
+    public GameObject pest;
+    public GameObject soilPos;
+
+    public Vector3 scaledSoil = new Vector3(379.7141f, 500f, 390.2904f); //soil lifted scaling
+
+    public float seconds;
+    public Boolean soilLifted;
+    public Boolean generatedTree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator Spawn()
+    {
+        seconds = UnityEngine.Random.Range(5f, 7f);
+
+        yield return new WaitForSeconds((int)seconds);
+        print(seconds);
+
+        pest.SetActive(true);
+
+    }
+
+    private void checkSoil()
+    {
+        if (soilPos.transform.lossyScale != scaledSoil)
+        {
+            soilLifted = false;
+
+        } else
+        {
+            soilLifted = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep updating to see if tree is generated in hierarchy
+        generatedTree = gameObject.GetComponentInChildren<LsystemScript>().generatedTree;
+
+        checkSoil();
+
+        //might need to check up on these again
+        if (generatedTree && !soilLifted)
+        {
+            if (!pest.activeInHierarchy)
+            {
+                StartCoroutine(Spawn());
+            }
+
+        } else
+        {
+            pest.SetActive(false);
+        }
+    }
+
+}
